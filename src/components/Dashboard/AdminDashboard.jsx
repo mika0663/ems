7441658efde5207121d99f20b56282a1eaf0f873
Muhammad Ikram <mikrams0663@gmail.com>
2025-04-06 18:12:25 +0500

    UI-Components-AdminDashboard
    TaskList UI almost compeleted
    Building UI for Admin Dashboard

---
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../Others/Header'
+const AdminDashboard = () => {
+  return (
+    <div className='h-screen w-full p-10'>
+      <Header />
+    </div>
+  )
+}
+
+export default AdminDashboard
--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -2,11 +2,40 @@ import React from 'react'
 
 function TaskList() {
   return (
-    <div className='h-[55%] flex gap-5 overflow-x-auto py-5 w-full flex-nowrap bg-red-400 mt-10 rounded-xl'>
-      <div className="flex-shrink-0 h-full  w-[300px] bg-yellow-400 rounded-xl"></div>
-      <div className="flex-shrink-0 h-full  w-[300px] bg-yellow-400 rounded-xl"></div>
-      <div className="flex-shrink-0 h-full  w-[300px] bg-yellow-400 rounded-xl"></div>
-      <div className="flex-shrink-0 h-full  w-[300px] bg-yellow-400 rounded-xl"></div>
+    <div id='tasklist' className='h-[55%] overflow-x-auto flex gap-5 py-5 w-full flex-nowrap  mt-10 rounded-xl'>
+      <div className="flex-shrink-0 h-full  w-[300px] p-5 bg-red-400 rounded-xl">
+        <div className='flex justify-between items-center'>
+            <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+            <h4 className='text-sm'>20 fed 2025</h4>
+            </div>
+            <h2 className='mt-5 text-2xl font-semibold'>Make a Youtube Video</h2>
+            <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.</p>
+        </div>
+      <div className="flex-shrink-0 h-full  w-[300px] p-5 bg-blue-400 rounded-xl">
+        <div className='flex justify-between items-center'>
+            <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+            <h4 className='text-sm'>20 fed 2025</h4>
+            </div>
+            <h2 className='mt-5 text-2xl font-semibold'>Make a Youtube Video</h2>
+            <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.</p>
+        </div>
+      <div className="flex-shrink-0 h-full  w-[300px] p-5 bg-green-400 rounded-xl">
+        <div className='flex justify-between items-center'>
+            <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+            <h4 className='text-sm'>20 fed 2025</h4>
+            </div>
+            <h2 className='mt-5 text-2xl font-semibold'>Make a Youtube Video</h2>
+            <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.</p>
+        </div>
+      <div className="flex-shrink-0 h-full  w-[300px] p-5 bg-yellow-400 rounded-xl">
+        <div className='flex justify-between items-center'>
+            <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+            <h4 className='text-sm'>20 fed 2025</h4>
+            </div>
+            <h2 className='mt-5 text-2xl font-semibold'>Make a Youtube Video</h2>
+            <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.</p>
+        </div>
+      
     </div>
   )
 }

73e2b680effff2e5a6ec6a1d4dd028c57763f339
Muhammad Ikram <mikrams0663@gmail.com>
2025-04-10 01:37:31 +0500

    adding dummy data to context

--- src/utils/localStorage.jsx
+++ src/utils/localStorage.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
-
-const localStorage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
-
-export default localStorage
+const employees = [
+          {
+            "id": 1,
+            "email": "john.doe@example.com",
+            "password": "123"
+          },
+          {
+            "id": 2,
+            "email": "jane.smith@example.com",
+            "password": "123"
+          },
+          {
+            "id": 3,
+            "email": "michael.johnson@example.com",
+            "password": "123"
+          },
+          {
+            "id": 4,
+            "email": "emily.williams@example.com",
+            "password": "123"
+          },
+          {
+            "id": 5,
+            "email": "david.brown@example.com",
+            "password": "123"
+          }
+        ]
+const admin = [
+    {
+        "id": 101,
+        "email": "admin@example.com",
+        "password": "123"
+      }
+]
